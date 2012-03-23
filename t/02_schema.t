24a523a6158d517cb5223a32af96e8bc459be85e
Mike Doherty <doherty@cs.dal.ca>
2012-03-22 21:34:31 -0400

    Fix off-by-one error causing line numbers to be misaligned

--- t/02_schema.t
+++ t/02_schema.t
@@ -9,7 +9,7 @@ my $paste = Paste->find({paste_id => 1});
 ok $paste, 'paste retrieved';
 can_ok $paste, qw(id content date deleted);
 is $paste->id, 1, 'correct ID';
-is $paste->content, 'test1', 'correct content';
+is $paste->content, "test1\nline2", 'correct content';
 isa_ok $paste->date, 'DateTime', 'date is inflated';
 cmp_ok $paste->date->compare( DateTime->now( time_zone => 'UTC' ) ), '<', 0, 'Paste created in the past';
 is $paste->deleted, 0, 'not deleted';
--- t/etc/schema.pl
+++ t/etc/schema.pl
@@ -9,7 +9,7 @@ use DateTime;
         basic => [
             Paste => [
                 [qw( paste_id paste_content paste_deleted paste_date )],
-                [1, 'test1', 0, DateTime->now( time_zone => 'UTC' )->subtract( minutes => 5 )],
+                [1, "test1\nline2", 0, DateTime->now( time_zone => 'UTC' )->subtract( minutes => 5 )],
                 [2, 'test2', 1, DateTime->now( time_zone => 'UTC' )->subtract( minutes => 5 )],
                 [3, '<omg>', 0, DateTime->now( time_zone => 'UTC' )->subtract( minutes => 4 )],
             ]
