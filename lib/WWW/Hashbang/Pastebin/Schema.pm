5bb46b3681e2f09c90b3306f08d28a689e272c33
Mike Doherty <doherty@cs.dal.ca>
2012-08-05 13:44:25 -0300

    Deploy if the configured DSN is SQLite in memory

--- lib/WWW/Hashbang/Pastebin/Schema.pm
+++ lib/WWW/Hashbang/Pastebin/Schema.pm
@@ -1,10 +1,41 @@
 package WWW::Hashbang::Pastebin::Schema;
 use strict;
 use warnings;
+use Try::Tiny;
 use base qw(DBIx::Class::Schema);
 # VERSION
 # ABSTRACT: Database schema for WWW::Hashbang::Pastebin
 
 __PACKAGE__->load_namespaces();
+
+sub connection {
+    my ($self, @info) = @_;
+    return $self if !@info && $self->storage;
+
+    my ($storage_class, $args) = ref $self->storage_type ?
+    ($self->_normalize_storage_type($self->storage_type),{}) : ($self->storage_type, {});
+
+    $storage_class = 'DBIx::Class::Storage'.$storage_class
+        if $storage_class =~ m/^::/;
+    try {
+        $self->ensure_class_loaded($storage_class);
+    }
+    catch {
+        $self->throw_exception(
+            "Unable to load storage class ${storage_class}: $_"
+        );
+    };
+    my $storage = $storage_class->new($self=>$args);
+    $storage->connect_info(\@info);
+    $self->storage($storage);
+
+    my $dbname = (split /:/, $info[0], 3)[2];
+    if ($dbname eq 'dbname=:memory:') {
+#        warn "deploying into $dbname";
+        $self->deploy();
+    }
+
+    return $self;
+}
 
 1;
